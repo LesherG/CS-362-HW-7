1efe47e5978c30864984c9aeecd2ddaecf6f2315
Gavin Lesher <lesherg@oregonstate.edu>
2021-03-07 14:47:24 -0800

    Question 1: test 3: added test for buzz functionality on multiples of 5

--- Question1/tests.py
+++ Question1/tests.py
@@ -7,6 +7,9 @@ class TestCode(unittest.TestCase):
         
     def test_fizz(self):
         self.assertEqual(code.fizz_buzz(2,4), '2, Fizz, 4')
+    
+    def test_buzz(self):
+        self.assertEqual(code.fizz_buzz(10,11), 'Buzz, 11')
         
 if __name__ == '__main__':
     
