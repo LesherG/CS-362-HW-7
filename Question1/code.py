d9f28b2de429f0b1ae4a354c8faf82499b8044e7
Gavin Lesher <lesherg@oregonstate.edu>
2021-03-07 14:45:28 -0800

    Question 1: implemented Fizz functionality for test 2

--- Question1/code.py
+++ Question1/code.py
@@ -2,8 +2,12 @@
 def fizz_buzz(num1, num2):
     ret = ""
     for i in range(num1, num2+1):
-        ret += str(i)
-        if(i != num2):
+        if(i != num1):
             ret += ", "
+        if(i % 3 == 0):
+            ret += 'Fizz'
+            continue
+        ret += str(i)
+        
             
     return(ret)
