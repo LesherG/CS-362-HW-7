75b9266b7de146b5e5a463cfe28c592d5cf2a301
Gavin Lesher <lesherg@oregonstate.edu>
2021-03-07 14:41:23 -0800

    Question 1: added test 2: testing for fizz on multiples of 3

--- Question1/tests.py
+++ Question1/tests.py
@@ -3,7 +3,10 @@ import code
 
 class TestCode(unittest.TestCase):
     def test_counting(self):
-        self.assertEqual(code.fizz_buzz(1,3), '1, 2, 3')
+        self.assertEqual(code.fizz_buzz(1,2), '1, 2')
+        
+    def test_fizz(self):
+        self.assertEqual(code.fizz_buzz(2,4), '2, Fizz, 4')
         
 if __name__ == '__main__':
     
