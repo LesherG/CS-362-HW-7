80a17dffaec7505c4c2f78009cc3a0756f3ad17c
Gavin Lesher <lesherg@oregonstate.edu>
2021-03-07 15:10:54 -0800

    Question 2: added functionality for test 3, mults of 400 return true

--- Question2/code.py
+++ Question2/code.py
@@ -3,6 +3,8 @@
 def leap_year(num):
     if(num % 4 == 0):
         if(num % 100 == 0):
+            if(num % 400 == 0):
+                return True
             return False
         return True
     else:
