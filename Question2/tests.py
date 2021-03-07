92653edc71241a01727cbe1d34f353f9076a25c2
Gavin Lesher <lesherg@oregonstate.edu>
2021-03-07 15:04:55 -0800

    Question 2: Test 2: tests if multiples of 4 that are not multiples of 100 return true

--- Question2/tests.py
+++ Question2/tests.py
@@ -7,6 +7,10 @@ class TestCode(unittest.TestCase):
         self.assertFalse(code.leap_year(3))
         self.assertFalse(code.leap_year(6))
         
+    def test_mult_of_4_non100(self):
+        self.assertTrue(code.leap_year(4))
+        self.assertTrue(code.leap_year(16))
+        
         
 if __name__ == '__main__':
     unittest.main()
