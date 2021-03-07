2b8926447222a3c09e27e34aeeace3d1e07c66a4
Gavin Lesher <lesherg@oregonstate.edu>
2021-03-07 14:49:19 -0800

    Question 1: added functionality for test 3, Buzz for multiples of 5

--- Question1/code.py
+++ Question1/code.py
@@ -4,10 +4,13 @@ def fizz_buzz(num1, num2):
     for i in range(num1, num2+1):
         if(i != num1):
             ret += ", "
+        
         if(i % 3 == 0):
             ret += 'Fizz'
-            continue
-        ret += str(i)
+        elif(i % 5 == 0):
+            ret += 'Buzz'
+        else:    
+            ret += str(i)
         
             
     return(ret)
